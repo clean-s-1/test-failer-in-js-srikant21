9faf3e877b866659b9e1f29ae5a5d833e66d8061
srikant21 <srikant21.k.sahoo@gmail.com>
2022-05-31 18:27:31 +0530

    Uploaded

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,20 @@
 const {expect} = require('chai')
+const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+function print_color_map(majorColors,minorColors) {
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            console.log(`${i * 5 + j + 1} | ${majorColors[i]} | ${minorColors[j]}`);
         }
-    }
+    }    
+}
+
+function print_color_length(majorColors,minorColors){
     return majorColors.length * minorColors.length;
 }
 
-result = print_color_map();
-expect(result).equals(25);
+result = print_color_map(majorColors,minorColors);
+printColorLength = print_color_length(majorColors,minorColors);
+expect(printColorLength).equals(25);
 console.log('All is well (maybe!)');
--- tshirts.js
+++ tshirts.js
@@ -10,7 +10,8 @@ function size(cms) {
     }
 }
 
-expect(size(37)).equals('S');
+expect(size(38)).equals('S');
+expect(size(39)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
 console.log('All is well (maybe!)');
